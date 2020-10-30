430746cf9a6f4de7e77ae007e55f586ac3c1863c
Satheno <eric.gedlich@freenet.de>
2020-10-30 13:13:03 +0100

    add generated test_set

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # vor = pv.compute_voronoi([[.1, .1, .1], [.9, .9, .9]], [[0, 1], [0, 1], [0, 1]], .2)
 
     gen = CurveDatasetGenerator(3, 10, 80, 4, .2, 20, 842)
-    gen.generate_and_save_dataset("../datasets/test_set/")
+    # gen.generate_and_save_dataset("../datasets/test_set/")
     dataset, description = gen.load_dataset("../datasets/test_set/")
     vis = CurveVisualizer(16, True, True)
     # vis.draw_original(dataset[0])
